03ead1233bcd01411f2d1eec8e52052d4ad16ac8
ThiagoKSR <thiagoklauber@gmail.com>
2024-10-22 12:00:09 -0300

    Add files via upload

---
+++ Aula01.py
@@ -0,0 +1,49 @@
+# lista = [1, 2, 3, 4, 5]
+# for item in lista:
+#     print(item, end=' ')
+
+
+# lista = [1, 5.2, True]
+# #nota = float(input('Digite a nota: '))
+# #lista[1] = nota
+# lista.append('Fortaleza')
+# lista.insert(0, 'Gramado')
+# lista.remove('Fortaleza')
+# lista.pop(1)
+# print(lista)
+
+
+
+# # Lista de compras
+# lista_de_compras = (
+#     "Leite",
+#     "Pão",
+#     "Ovos",
+#     "Café",
+#     "Queijo",
+#     "Manteiga",
+#     "Açúcar",
+#     "Arroz",
+#     "Feijão",
+#     "Frutas"
+# )
+
+# lista = ('arroz', 'feijao', 'maça', 'batata')
+# indice = 0
+
+# print('LISTA DE COMPRAS', end='\n\n')
+
+# for item in lista:
+#     if item[2] == 'maça':
+#         print(f'O indice da maça é: {indice}')
+#     else:
+#         indice += 1
+
+# print(f'O indice da maça pelo metodo é: {lista.count('maça')}')
+
+
+participantes = (['Thiago', 'Python', 'Uninassau', 
+                  'Ronaldo', 'HTML', 'Uniasselv', 
+                  'Soldado', 'Musculação', 'Skyfit'])
+
+print(f'O participante {participantes[3]} irá falar de {participantes[4]} pela instituição {participantes[5]}')
