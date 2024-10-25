887bc0bb0c04b9a78ed8083ad1d78d3ecb35b460
ThiagoKSR <thiagoklauber@gmail.com>
2024-10-25 19:35:30 -0300

    Add files via upload

---
+++ ProvaPYIA.py
@@ -0,0 +1,7 @@
+#Crie uma lista contendo seis frutas de sua escolha.
+#Depois de ter a lista pronta, converta essa lista em uma tupla.
+#Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+frutas = ['laranja', 'maça', 'banana', 'ameixa', 'kiwi', 'carambola']
+tupla_frutas = tuple(frutas)
+print(tupla_frutas)
