4fd88f7aca17e2f78762f358c8d3e74e78e1ab38
ThiagoKSR <thiagoklauber@gmail.com>
2024-10-29 11:54:37 -0300

    Add files via upload
    Exemplos utilizados na aula 02 do modulo 02 PYIA. 
    Sets e Dicionários.

---
+++ PYIAAula02.py
@@ -0,0 +1,54 @@
+# frutas = set()
+# frutas.update(['maça', 'banana', 'uva', 'laranja', 'morango'])
+# print('banana' in frutas)
+
+
+# frutas_vermelhas = set()
+# frutas_vermelhas.update(['morango', 'cereja', 'framboesa'])
+# frutas_vermelhas.discard('cereja')
+# print(frutas_vermelhas)
+
+
+
+# conjunto_A = {'1', '2', '3'}
+# conjunto_B = {'x', 'y', 'z'}
+# print(conjunto_A.union(conjunto_B))
+
+
+
+# conjunto_A = {'1', '2', '3'}
+# conjunto_B = {'2', '5', '7'}
+# print(conjunto_A.intersection(conjunto_B))
+
+
+
+# lista_A = ['Maria', 'João', 'Pedro']
+# lista_B = ['Thiago','João','Guilherme']
+# uniao = set(lista_A)
+# uniao_2 = set(lista_B)
+# print(uniao.union(uniao_2))
+
+
+
+# my_dict = {'nome': 'Thiago',
+#            'idade': 31,
+#            'Curso': 'Desenvolvimento Full Stack'}
+# for elementos, valor in my_dict.items():
+#     print(f'{elementos} : {valor}')
+
+
+
+# notas = {'nome': input('Digite o nome: '),
+#          'idade': input('Digite a idade: '),
+#          'disciplina': input('digite a disciplina: ')
+#          }
+# for elemento, valor in notas.items():
+#     print(f'{elemento} : {valor}')
+
+
+# notas = {'nome': 'Junior',
+#          'idade': input('Digite a idade: '),
+#          'disciplina': input('digite a disciplina: ')}
+
+# notas['nome'] = input('Digite o nome: ')
+# print(notas)
